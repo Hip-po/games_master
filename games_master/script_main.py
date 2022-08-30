ff59e1bd0173dc3df363ef48375614419cc7a361
Camila Mallmann <camila-mallmann@hotmail.com>
2022-08-30 11:38:41 +0200

    adding comments to script

--- games_master/script_main.py
+++ games_master/script_main.py
@@ -8,6 +8,10 @@ EPSILON = 0.1
 ACT_RANGE = 5
 
 def parse_obs(obs):
+    '''
+    converts into a tensor.
+    '''
+    #rearrange index in order to have channel first
     return torch.permute(torch.tensor(obs, dtype=torch.float), (2, 0, 1)).unsqueeze(0)
 
 def policy(new_obs):
@@ -18,9 +22,9 @@ def policy(new_obs):
 
 def learn(old_obs, action, new_obs, reward):
 
-    out = agt(old_obs).squeeze(0)[action]
-    with torch.no_grad():
-        exp = reward + GAMMA * agt(new_obs).max()
+    out = agt(old_obs).squeeze(0)[action] #equivalent to y_pred
+    with torch.no_grad():                 #don't use backward propagation for calculate y_true
+        exp = reward + GAMMA * agt(new_obs).max()   ##y_true with Q learning formula
 
     loss = torch.square(exp - out)
     print(action, reward, loss)
@@ -34,7 +38,7 @@ class ImageDQN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.net = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 20, 2),
+            torch.nn.Conv2d(3, 20, 2),  #input_channels=3, filters = 20, kernel_size =2
             torch.nn.ReLU(inplace=True),
             torch.nn.MaxPool2d(2),
             torch.nn.ReLU(inplace=True),
@@ -45,7 +49,7 @@ class ImageDQN(torch.nn.Module):
             torch.nn.Flatten(start_dim=0),
             torch.nn.Linear(20*529, 1024),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(1024, 5),
+            torch.nn.Linear(1024, ACT_RANGE), # 1024 is input_dim
         )
 
     def forward(self, X):
