45a6d461bb2840d30a51a99acd1fc4440b9d1b6b
Hippolyte Lefer <hippolytelefer@gmail.com>
2022-08-30 11:27:34 +0200

    start project

--- games_master/script_Maxime.py
+++ games_master/script_Maxime.py
@@ -1,21 +1,52 @@
 import gym
 import torch
 import matplotlib.pyplot as plt
+import random
+
+GAMMA = 0.98
+EPSILON = 0.1
+ACT_RANGE = 5
 
 def parse_obs(obs):
     return torch.permute(torch.tensor(obs, dtype=torch.float), (2, 0, 1)).unsqueeze(0)
+
+def policy(new_obs):
+    if random.uniform(0, 1) < EPSILON:
+        return random.randint(0, ACT_RANGE - 1)
+    with torch.no_grad():
+        return torch.argmax(agt(new_obs)).numpy()
+
+def learn(old_obs, action, new_obs, reward):
+
+    out = agt(old_obs).squeeze(0)[action]
+    with torch.no_grad():
+        exp = reward + GAMMA * agt(new_obs).max()
+
+    loss = torch.square(exp - out)
+    print(action, reward, loss)
+
+    opt.zero_grad()
+    loss.sum().backward()
+    opt.step()
 
 class ImageDQN(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
         self.net = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 20, 1),
+            torch.nn.Conv2d(3, 20, 2),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Flatten(),
-            torch.nn.Linear(96*96*20, 5)
+            torch.nn.MaxPool2d(2),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(20, 20, 2),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(2),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Flatten(start_dim=0),
+            torch.nn.Linear(20*529, 1024),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(1024, ACT_RANGE),
         )
-        pass
 
     def forward(self, X):
         y = self.net(X)
@@ -25,31 +56,21 @@ agt = ImageDQN()
 opt = torch.optim.Adam(agt.net.parameters(), lr=0.0001)
 env = gym.make("CarRacing-v2", continuous=False)
 
+
 new_obs = parse_obs(env.reset())
 
 for _ in range(10000):
 
-    val = agt(new_obs)
-    act = torch.argmax(val).numpy()
-    old_obs = new_obs
-    print(val, act)
+    action = policy(new_obs)
 
-    new_obs, reward, done, info = env.step(act)
+    old_obs = new_obs
+
+    new_obs, reward, done, info = env.step(action)
     new_obs = parse_obs(new_obs)
+    learn(old_obs, action, new_obs, reward)
 
     env.render()
-
-    out = val.squeeze(0)[act]
-    with torch.no_grad():
-        exp = reward + 0.98 * agt(new_obs).max()
-
-    loss = torch.square(exp - out)
-
-    opt.zero_grad()
-    loss.sum().backward()
-    opt.step()
 
     if done:
         env.reset()
 env.close()
-
