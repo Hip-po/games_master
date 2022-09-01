b7eb7a91365c83a1d5b0bf067162eac2deadd53f
Hippolyte Lefer <hippolytelefer@gmail.com>
2022-09-01 12:14:59 +0200

    organise files

---
+++ games_master/car_racing/main.py
@@ -0,0 +1,14 @@
+from agent import *
+from environment import *
+from config import CFG
+from manual import manual
+
+CFG.init()
+
+agt = ImageDQNagent()
+env = get_env()
+
+if CFG.MANUAL:
+    manual(env,agt)
+
+run_env(env,agt)
